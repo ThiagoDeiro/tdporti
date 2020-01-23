3b5fb1fdcc920e67378469c3fd7a575dfedffc70
ThiagoDeiro <thiagodeiro.11@hotmail.com>
2020-01-23 14:52:24 +1100

    Updates

---
+++ precache-manifest.06177ef18aa754e722d6fc540ea031ff.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8765e510d3810ded74129f209073ece5",
+    "url": "/tdporti/index.html"
+  },
+  {
+    "revision": "69c5404794415b27a15f",
+    "url": "/tdporti/static/css/main.1786bc5e.chunk.css"
+  },
+  {
+    "revision": "016ac4c63e9c69ed767a",
+    "url": "/tdporti/static/js/2.924d809b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/tdporti/static/js/2.924d809b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69c5404794415b27a15f",
+    "url": "/tdporti/static/js/main.28a92fd4.chunk.js"
+  },
+  {
+    "revision": "68b1099d8a58442d97af",
+    "url": "/tdporti/static/js/runtime-main.55a4754c.js"
+  },
+  {
+    "revision": "4e61fbd9e0969e0687b86b3bcaa4bfd4",
+    "url": "/tdporti/static/media/NameImg.4e61fbd9.jpg"
+  },
+  {
+    "revision": "fdbe641688123d80926b8b1d16d369bb",
+    "url": "/tdporti/static/media/NavImg.fdbe6416.jpg"
+  },
+  {
+    "revision": "f1374535535dfbcd4fddd06085c051d7",
+    "url": "/tdporti/static/media/Thiago.f1374535.jpg"
+  },
+  {
+    "revision": "2a6f37b258d146a6e88110b6b601d88f",
+    "url": "/tdporti/static/media/mealpicker.2a6f37b2.png"
+  },
+  {
+    "revision": "f8d965e5651877deac7e54b209d0f2b3",
+    "url": "/tdporti/static/media/projectRuby.f8d965e5.png"
+  },
+  {
+    "revision": "a7ea7c0cfd8b5035136df3a07b2ee23c",
+    "url": "/tdporti/static/media/resume.a7ea7c0c.pdf"
+  },
+  {
+    "revision": "4ddf5a223494f903fe3049234414ed4a",
+    "url": "/tdporti/static/media/todolist2.4ddf5a22.png"
+  }
+]);
